5fafae94b4eff252813bd19208731ae43499c17f
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-08-19 22:24:28 +0200

    feat: :sparkles: add task component

--- src/List.jsx
+++ src/List.jsx
@@ -1,3 +1,6 @@
+import { GrTrash } from 'react-icons/gr';
+import { AiFillCheckSquare } from 'react-icons/ai';
+
 function List() {
   return (
     <div className="w-full max-w-3xl mx-auto mt-24 mb-12 px-4">
@@ -17,10 +20,24 @@ function List() {
       </div>
 
       <div className="flex flex-col gap-3">
-        <p>task</p>
-        <p>task</p>
-        <p>task</p>
+        <Task />
+        <Task />
+        <Task />
       </div>
+    </div>
+  );
+}
+
+function Task() {
+  return (
+    <div className="w-full border-2 border-slate-800 p-4 flex place-items-center justify-between gap-3">
+      <button>
+        <AiFillCheckSquare />
+      </button>
+      <p className="text-sm mr-auto">task</p>
+      <button>
+        <GrTrash size={20} />
+      </button>
     </div>
   );
 }
