baef1bca55c798d6326d340227229cb4671ccdbd
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-08-19 23:28:36 +0200

    feat: :lipstick: add ui animations

--- src/List.jsx
+++ src/List.jsx
@@ -1,9 +1,12 @@
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 import { GrTrash } from 'react-icons/gr';
 import { AiFillCheckSquare } from 'react-icons/ai';
 
 function List({ tasks, onComplete, onRemove }) {
   const taskQuantity = tasks.length;
   const completedTasks = tasks.filter((task) => task.isComplete).length;
+
+  const [parent] = useAutoAnimate();
 
   return (
     <div className="w-full max-w-3xl mx-auto mt-24 mb-12 px-4">
@@ -22,7 +25,7 @@ function List({ tasks, onComplete, onRemove }) {
         </div>
       </div>
 
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-3" ref={parent}>
         {tasks.map((task) => (
           <Task
             key={task.id}
