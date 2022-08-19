801ebc4450cf43f81b77c9fc7705f67a15402adb
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-08-19 23:05:16 +0200

    feat: :sparkles: add task counters

--- src/List.jsx
+++ src/List.jsx
@@ -2,19 +2,22 @@ import { GrTrash } from 'react-icons/gr';
 import { AiFillCheckSquare } from 'react-icons/ai';
 
 function List({ tasks, onComplete }) {
+  const taskQuantity = tasks.length;
+  const completedTasks = tasks.filter((task) => task.isComplete).length;
+
   return (
     <div className="w-full max-w-3xl mx-auto mt-24 mb-12 px-4">
       <div className="flex place-items-center place-content-between mb-6">
         <div className="flex place-items-center gap-2">
           <p className="text-sm font-bold">Created</p>
           <span className="text-xs font-bold bg-slate-800 text-slate-100 px-2 py-1">
-            5
+            {taskQuantity}
           </span>
         </div>
         <div className="flex place-items-center gap-2">
           <p className="text-sm font-bold">Completed</p>
           <span className="text-xs font-bold bg-slate-800 text-slate-100 px-2 py-1">
-            2 of 5
+            {completedTasks} of {taskQuantity}
           </span>
         </div>
       </div>
