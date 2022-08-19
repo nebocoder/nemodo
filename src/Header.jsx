510d04fb2d2c6c77914336971ff3f49e47ee0618
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-08-19 22:04:12 +0200

    feat: :lipstick: add icons to header component

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,13 @@
+import { AiOutlinePlus } from 'react-icons/ai';
+import { ImCheckmark } from 'react-icons/im';
+
 function Header() {
   return (
     <div className="flex place-content-center place-items-center h-36 relative">
-      <div className="text-3xl font-bold">nemodo</div>
+      <div className="text-5xl font-bold flex place-items-center gap-2">
+        <ImCheckmark size={48} />
+        nemodo
+      </div>
       <form className="absolute h-14 -bottom-14 w-full max-w-3xl flex px-4">
         <input
           className="h-full flex-1 text-inherit border-y-2 border-l-2 bg-inherit border-slate-800 px-4
@@ -9,7 +15,10 @@ function Header() {
           type="text"
           placeholder="Add a new task..."
         />
-        <button className="h-full px-4 border-2 border-slate-800">Add</button>
+        <button className="h-full px-4 border-2 border-slate-800 font-bold flex place-items-center gap-2">
+          Add
+          <AiOutlinePlus size={22} />
+        </button>
       </form>
     </div>
   );
