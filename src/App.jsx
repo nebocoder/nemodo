762275172e9d10b68f81eb511182f0b9aeeff934
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-08-27 22:40:27 +0200

    refactor: :art: rename dark component into footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { nanoid } from 'nanoid';
 import { useEffect, useState } from 'react';
-import Dark from './components/Dark';
+import Dark from './components/Footer';
 import Header from './components/Header';
 import List from './components/List';
 
